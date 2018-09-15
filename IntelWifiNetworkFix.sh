6ee8d722f963040ec2787af62d0172940ea1fd76
Kurazs Iosif Daniel <iosif.kurazs@intel.com>
2018-09-15 12:39:56 +0300

    Script with workaround to enable intel wifi adapter at boot
    Put this file in the /etc/profile.d/ folder for Ubuntu to be executed a login

---
+++ IntelWifiNetworkFix.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# configuration variables:
+TOTAL_ALLOWED_RUNS=5
+TIME_BETWEEN_REMOVE_COMMANDS=5s
+TIME_BETWEEN_ADDING_COMMANDS=5s
+TIME_AFTER_ADDING_BEFORE_CHECKING_IF_WIFI_WORKS=10s
+readonly TOTAL_ALLOWED_RUNS
+
+# internal used variables:
+CURRENT_RUN=0
+TOTAL_RUNS=0
+
+#repeate until wifi available
+while [ false ]
+do
+  echo "Removing kernel modules iwl4965,iwl3945 and iwlwifi..."
+  # first remove all the wifi kernel drivers
+  (modprobe iwl4965 -r 2>&1) > /dev/null
+  (modprobe iwl3945 -r 2>&1) > /dev/null
+  (modprobe iwlwifi -r 2>&1) > /dev/null
+
+  sleep $TIME_BETWEEN_REMOVE_COMMANDS
+  echo "    kernel modules removed."
+
+  if [ $CURRENT_RUN -eq 0 ]; then
+      CURRENT_RUN=1
+      # Now try to add and repeat if it fails
+      echo "Adding kernel module iwl4965"
+      modprobe iwl4965
+      sleep $TIME_BETWEEN_REMOVE_COMMANDS
+      echo "Adding kernel module iwl3945"
+      modprobe iwl3945
+      # This needs to be added last.
+      sleep $TIME_BETWEEN_REMOVE_COMMANDS
+      echo "Adding kernel module iwlwifi"
+      modprobe iwlwifi
+  else
+      CURRENT_RUN=0
+      # Now try to add and repeat if it fails
+      echo "Adding kernel module iwl3945"
+      modprobe iwl3945
+      sleep $TIME_BETWEEN_ADDING_COMMANDS
+      echo "Adding kernel module iwl4965"
+      modprobe iwl4965
+      # This needs to be added last.
+      sleep $TIME_BETWEEN_ADDING_COMMANDS
+      echo "Adding kernel module iwlwifi"
+      modprobe iwlwifi
+  fi
+
+  echo "Wait $TIME_AFTER_ADDING_BEFORE_CHECKING_IF_WIFI_WORKS and then check if wifi adapter is available"
+  sleep $TIME_AFTER_ADDING_BEFORE_CHECKING_IF_WIFI_WORKS
+
+  CONDITION=`( iwconfig 2>&1 ) | grep -Po '^((?!\S+\s+no wireless extensions)\S*)'`
+
+  if [ ! -z $CONDITION ]; then
+    echo "WIFI ENABLED SUCCESSFULLY !"
+    break
+  else
+    if [ $TOTAL_RUNS -eq $TOTAL_ALLOWED_RUNS ]; then
+       echo "SCRIPT FAILED !!! CRITICAL ERROR , requires manual investigation"
+       exit -1
+    fi
+    TOTAL_RUNS=`expr $TOTAL_RUNS + 1`
+    echo $CONDITION
+    echo "FAILED RETRYING..."
+  fi
+done
